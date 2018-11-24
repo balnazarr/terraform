b4a59ee90e8c8546f3e008cc9f72fecd04cdd5ad
danny <dbrody@danny.com>
2018-11-23 22:08:20 -0800

    gitignore

--- modules/mod-vpc/main.tf
+++ modules/mod-vpc/main.tf
@@ -1,6 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+locals{
+   vpc_cidr = "10.0.0.0/16"
 
+   general = "cidrsubnet(${local.vpc_cidr}, 6,2 )"
+   access  = "cidrsubnet(${local.vpc_cidr}, 6,1 )"
+   database = "cidrsubnet(${local.vpc_cidr}, 6,3 )"
+
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "${local.vpc_cidr}"
 
   tags {
       Name = "${var.name}-VPC"
