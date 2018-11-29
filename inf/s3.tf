99215e6a96779c8dae8d9501acc1216c38091de6
danny <dbrody@danny.com>
2018-11-28 23:25:29 -0800

    s3 updates

--- inf/s3.tf
+++ inf/s3.tf
@@ -3,9 +3,33 @@ resource "random_integer" "rnd" {
   max     = 600
 }
 
-resource "aws_s3_bucket" "test" {
-  bucket = "${var.s3_name}-${random_integer.rnd.result}"
+resource "random_integer" "rnd2" {
+  min     = 800
+  max     = 900
+}
+
+resource "aws_s3_bucket" "source" {
+  bucket = "${var.s3_src}-${random_integer.rnd.result}"
   region = "${var.region}"  
 
   acl = "private"
 }
+
+resource "aws_s3_bucket" "target" {
+  bucket = "${var.s3_dest}-${random_integer.rnd2.result}"
+  region = "${var.region}"
+
+  acl = "private"
+}
+
+/*
+resource "aws_s3_bucket_notification" "event" {
+  bucket = "${aws_s3_bucket.test.id}"
+
+  lambda_function {
+    lambda_function_arn = "${aws_lambda_function.func.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".txt"
+  }
+}
+*/
