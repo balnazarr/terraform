2892acf99748000862c8ebbf0bbd4ca55373c240
danny <dbrody@danny.com>
2018-11-28 22:50:00 -0800

    -s3 added

---
+++ inf/terraform.tfvars
@@ -0,0 +1,4 @@
+region = "us-west-2"
+vpc_cidr = "10.0.0.0/16"
+
+s3_name = "danny-test"
--- inf/variables.tf
+++ inf/variables.tf
@@ -1,2 +1,3 @@
-variable "region" { default = "us-west-2" }
-variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "region" {}
+variable "vpc_cidr" {}
+variable "s3_name" {}
