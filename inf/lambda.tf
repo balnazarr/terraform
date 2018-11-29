b1ced628e35d1ade495da4f8f1b470a7b45866f2
danny <dbrody@danny.com>
2018-11-28 23:55:33 -0800

    s3 event created

--- inf/lambda.tf
+++ inf/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "lambda" {
   filename         = "danny-lambda.zip"
-  function_name    = "lambda_function"
+  function_name    = "danny-s3-copy"
   role             = "${aws_iam_role.lambda.arn}"
-  handler          = "exports.test"
+  handler          = "lambda_function.lambda_handler"
   source_code_hash = "${base64sha256(file("danny-lambda.zip"))}"
   runtime          = "python3.6"
 }
